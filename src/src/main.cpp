5e9bde8d78f9e58ded4936fb11fca4ea0fdb9bf3
drj45115 <josef.draexl@st.oth-regensburg.de>
2024-05-07 19:37:18 +0000

    ordered maps to speed up heuristic optimization algorithm

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -41,25 +41,25 @@ private:
 std::string root_directory = "/home/josefdra/ZOCK/g01";
 
 std::array<std::tuple<std::string, uint8_t>, 19> maps{
-    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2012_Map_fuenf.map", 3),
-    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2013_comp_1_2p.map", 2),
-    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2013_comp_4_2p.map", 2),
-    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2013_comp_5_2p.map", 2),
+    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/comp2020_02_2p.map", 2),
+    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2019_comp_05_2p.map", 2),
     std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2013_comp_9_2.map", 2),
+    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2012_Map_fuenf.map", 3),
+    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/comp2020_02_4p.map", 4),
+    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2013_comp_5_2p.map", 2),
+    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2013_comp_4_2p.map", 2),
+    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2019_comp_07_4p.map", 4),
+    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2019_comp_03_4p.map", 4),
     std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2014_comp_1_4p.map", 4),
     std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2014_comp_2_2p.map", 2),
     std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2014_comp_2_4p.map", 4),
-    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2014_comp_3_8p.map", 8),
-    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2019_comp_03_4p.map", 4),
-    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2019_comp_05_2p.map", 2),
-    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2019_comp_07_4p.map", 4),
+    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/comp2020_02_3p.map", 3),
     std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/comp2017_02_3p.map", 3),
+    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2013_comp_1_2p.map", 2),
     std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/comp2019_01_4p.map", 4),
     std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/comp2020_01_8p.map", 8),
-    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/comp2020_02_2p.map", 2),
-    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/comp2020_02_3p.map", 3),
-    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/comp2020_02_4p.map", 4),
-    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/comp2020_02_8p.map", 8)};
+    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/comp2020_02_8p.map", 8),
+    std::tuple<std::string, uint8_t>(root_directory + "/automated_testing/maps/2014_comp_3_8p.map", 8)};
 
 int total_games(71 * 25 * 25 * 25);
 int total_finished_games(0);
@@ -67,9 +67,12 @@ uint8_t most_won_games(0);
 uint8_t best_mult1(1);
 uint8_t best_mult2(1);
 uint8_t best_mult3(1);
-uint8_t mult1(1);
-uint8_t mult2(1);
-uint8_t mult3(1);
+uint8_t curr_mult1(1);
+uint8_t curr_mult2(1);
+uint8_t curr_mult3(1);
+uint8_t glob_mult1(1);
+uint8_t glob_mult2(1);
+uint8_t glob_mult3(1);
 std::atomic<uint8_t> finished_games(0);
 std::atomic<uint8_t> won_games(0);
 bool finished(false);
@@ -141,7 +144,7 @@ void run_game(std::string &map_path, uint8_t mult1, uint8_t mult2, uint8_t mult3
 {
     if (!stop)
     {
-        Game game(map_path, best_mult1, best_mult2, best_mult3, mult1, mult2, mult3, position);
+        Game game(map_path, curr_mult1, curr_mult2, curr_mult3, mult1, mult2, mult3, position);
         bool result = game.run(stop);
         if (result)
         {
@@ -153,47 +156,44 @@ void run_game(std::string &map_path, uint8_t mult1, uint8_t mult2, uint8_t mult3
 
 void run_match(uint8_t mult1, uint8_t mult2, uint8_t mult3, ThreadPool &pool)
 {
-    bool stop = false;
-    for (auto &map : maps)
+    if (won_games != 71)
     {
-        std::string map_path;
-        uint8_t player_count;
-        std::tie(map_path, player_count) = map;
-
-        for (uint8_t p = 0; p < player_count; p++)
+        bool stop = false;
+        for (auto &map : maps)
         {
-            pool.enqueue(run_game, map_path, mult1, mult2, mult3, p, stop);
+            std::string map_path;
+            uint8_t player_count;
+            std::tie(map_path, player_count) = map;
+
+            for (uint8_t p = 0; p < player_count; p++)
+            {
+                pool.enqueue(run_game, map_path, mult1, mult2, mult3, p, stop);
+            }
         }
+        std::cout << "queued all 71 games" << std::endl;
+        while ((finished_games.load() != 71) && (((71 - finished_games.load()) + won_games.load()) > most_won_games))
+        {
+            usleep(5000);
+        }
+        total_finished_games += 71;
+        if (!(((71 - finished_games.load()) + won_games.load()) > most_won_games))
+        {
+            std::cout << "stopped remaining games, because most_won_games could not be bet" << std::endl;
+        }
+        else
+        {
+            std::cout << "won " << (int)won_games.load() << "/71 games with configuration: " << (int)mult1 << " " << (int)mult2 << " " << (int)mult3 << std::endl;
+        }
+        if (won_games.load() > most_won_games)
+        {
+            most_won_games = won_games;
+            best_mult1 = mult1;
+            best_mult2 = mult2;
+            best_mult3 = mult3;
+        }
+        finished_games.store(0);
+        won_games.store(0);
     }
-    std::cout << "queued all 71 games" << std::endl;
-    while ((finished_games.load() != 71) && (((71 - finished_games.load()) + won_games.load()) > most_won_games))
-    {
-        usleep(5000);
-    }
-    total_finished_games += 71;
-    if (!(((71 - finished_games.load()) + won_games.load()) > most_won_games))
-    {
-        std::cout << "stopped remaining games, because most_won_games could not be bet" << std::endl;
-    }
-    else
-    {
-        std::cout << "won " << (int)won_games.load() << "/71 games with configuration: " << (int)mult1 << " " << (int)mult2 << " " << (int)mult3 << std::endl;
-    }
-    if (won_games.load() > most_won_games)
-    {
-        most_won_games = won_games;
-        best_mult1 = mult1;
-        best_mult2 = mult2;
-        best_mult3 = mult3;
-        mult1 = 1;
-        mult2 = 1;
-        mult3 = 1;
-        total_finished_games = 0;
-        std::cout << "Restarting with these best values: " << (int)best_mult1 << " " << (int)best_mult2 << " " << (int)best_mult3 << " winning " << (int)most_won_games << std::endl;
-        return;
-    }
-    finished_games.store(0);
-    won_games.store(0);
     if (total_finished_games == total_games)
     {
         finished = true;
@@ -207,28 +207,56 @@ int main()
     auto start_time = std::chrono::steady_clock::now();
     ThreadPool pool(threads);
     uint8_t variations = 26;
-    for (uint8_t mult1 = 1; mult1 < variations; mult1++)
+    while (1)
     {
-        for (uint8_t mult2 = 1; mult2 < variations; mult2++)
+        for (glob_mult1 = 1; glob_mult1 < variations; glob_mult1++)
         {
-            for (uint8_t mult3 = 1; mult3 < variations; mult3++)
+            if (most_won_games == 71)
             {
-                std::cout << "Starting match " << (int)(25 * 25 * mult1 - 25 * 25 + 25 * mult2 - 25 + mult3) << " with this configuration: " << (int)mult1 << " " << (int)mult2 << " " << (int)mult3 << std::endl;
-                std::cout << "current best configuration: " << (int)best_mult1 << " " << (int)best_mult2 << " " << (int)best_mult3 << " winning " << (int)most_won_games << " games" << std::endl;
-                std::cout << std::setprecision(5) << (float)total_finished_games / float(total_games) << "% done" << std::endl;
-                // one match is all maps with all player positions
-                run_match(mult1, mult2, mult3, pool);
+                break;
+            }
+            for (glob_mult2 = 1; glob_mult2 < variations; glob_mult2++)
+            {
+                if (most_won_games == 71)
+                {
+                    break;
+                }
+                for (glob_mult3 = 1; glob_mult3 < variations; glob_mult3++)
+                {
+                    if (most_won_games == 71)
+                    {
+                        break;
+                    }
+                    std::cout << "Starting match " << (int)(25 * 25 * glob_mult1 - 25 * 25 + 25 * glob_mult2 - 25 + glob_mult3) << " with this configuration: " << (int)glob_mult1 << " " << (int)glob_mult2 << " " << (int)glob_mult3 << std::endl;
+                    std::cout << "current best configuration: " << (int)best_mult1 << " " << (int)best_mult2 << " " << (int)best_mult3 << " winning " << (int)most_won_games << " games" << std::endl;
+                    std::cout << std::setprecision(5) << (float)total_finished_games / float(total_games) << "% done" << std::endl;
+                    // one match is all maps with all player positions
+                    run_match(glob_mult1, glob_mult2, glob_mult3, pool);
+                }
             }
         }
+        while (!finished)
+        {
+            sleep(1);
+        }
+        auto end_time = std::chrono::steady_clock::now();
+        auto seconds = std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time);
+        std::cout << "The best configuration was: " << (int)best_mult1 << " " << (int)best_mult2 << " " << (int)best_mult3 << " winning " << (int)most_won_games << " games" << std::endl;
+        std::cout << "Elapsed time: " << seconds.count() << " seconds" << std::endl;
+        if (most_won_games == 0)
+        {
+            break;
+        }
+        else
+        {
+            most_won_games = 0;
+        }
+        curr_mult1 = best_mult1;
+        curr_mult2 = best_mult2;
+        curr_mult3 = best_mult3;
+        most_won_games = 0;
+        std::cout << "Starting again with enemy configuration: " << (int)curr_mult1 << " " << (int)curr_mult2 << " " << (int)curr_mult3 << std::endl;
     }
-    while (!finished)
-    {
-        sleep(1);
-    }
-    auto end_time = std::chrono::steady_clock::now();
-    auto seconds = std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time);
-    std::cout << "The best configuration was: " << (int)best_mult1 << " " << (int)best_mult2 << " " << (int)best_mult3 << " winning " << (int)most_won_games << " games" << std::endl;
-    std::cout << "Elapsed time: " << seconds.count() << " seconds" << std::endl;
     fclose(stdout);
     return 0;
 }
