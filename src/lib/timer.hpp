8dd49dd333e04029e8c5013b493616be31186ac5
drj45115 <josef.draexl@st.oth-regensburg.de>
2024-05-30 10:03:21 +0000

    updated timer

--- src/lib/timer.hpp
+++ src/lib/timer.hpp
@@ -40,7 +40,7 @@ public:
         return duration.count();
     }
 
-    uint32_t exception_time = 10000;
+    uint32_t exception_time = 20000;
 
 private:
     std::chrono::time_point<std::chrono::high_resolution_clock> m_start_time;
