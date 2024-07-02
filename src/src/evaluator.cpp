cff60fc53e3838db3d1b8a821f1e699bf597fa14
Josef Dräxl <josef.draexl@st.oth-regensburg.de>
2024-07-02 08:57:35 +0200

    added statistics back in

--- src/src/evaluator.cpp
+++ src/src/evaluator.cpp
@@ -2,6 +2,7 @@
 #include "board.hpp"
 #include "move_generator.hpp"
 #include "timer.hpp"
+#include "statistics.hpp"
 
 void print_static_evaluation(Board &board)
 {
@@ -25,6 +26,8 @@ int get_static_eval(Board &board, std::bitset<MAX_NUM_OF_FIELDS> &community_play
 
 int get_evaluation(Board &board, uint8_t player_num, Timer &timer, MoveGenerator &move_gen, uint8_t index)
 {
+    Timer evaluation_time;
+    leafs_calculated++;
     // print_static_evaluation(board);
     // exit(0);
 
@@ -104,6 +107,7 @@ int get_evaluation(Board &board, uint8_t player_num, Timer &timer, MoveGenerator
 
         // std::cout << "Score: " << score << std::endl;
 
+        average_evaluation_time += evaluation_time.get_elapsed_time();
         return score;
     }
     catch (const TimeLimitExceededException &)
